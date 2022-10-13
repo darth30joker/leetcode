4784ce676ac5c98794f575144bf5d03e20ce3491
Darth Joker <darthjoker@outlook.ie>
2022-10-13 16:48:04 +0100

    solve a new problem.

---
+++ python/contains_duplicate.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                return True
+
+        return False
+
+
+solution = Solution()
+print(solution.containsDuplicate([1, 2, 3, 1]))
+print(solution.containsDuplicate([1, 2, 3, 4]))
